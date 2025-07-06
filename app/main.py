e28bd643dd21d910ecf02fb42ed7cd57a00f2e68
hboussouar <hichem.boussouar@gmail.com>
2025-07-06 22:46:19 +0200

    ajout dockerfile/modif6

--- app/main.py
+++ app/main.py
@@ -62,3 +62,10 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+if __name__ == "__main__":
+    import os
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
