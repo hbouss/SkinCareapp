c9d24d930c2c77dd9507370a3cee81a93899f1ed
hboussouar <hichem.boussouar@gmail.com>
2025-07-06 23:10:27 +0200

    ajout dockerfile/modif8

--- app/main.py
+++ app/main.py
@@ -67,5 +67,11 @@ if __name__ == "__main__":
     import os
     import uvicorn
 
+    # récupère PORT depuis l'env (Railway injecte automatiquement), sinon 8000
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
+    uvicorn.run(
+        "app.main:app",            # module:app
+        host="0.0.0.0",
+        port=port,
+        log_level="info"
+    )
